f9e3503962fa0d923957e09ed507d8df960377a4
Martinho Sebastião <martinho.sebastiao@outlook.com>
2023-10-17 18:05:01 +0100

    Reconfiguração para Minimal API
    Ajuste dos directorios a ser usado, em conformidade com a Clean Arquitecture

---
+++ IBGE.Api/Domain/Entities/State.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace IBGE.Api.Domains.Entities
+{
+	public class State
+	{
+		private readonly List<Town> towns;
+
+		protected State() { }
+        public State(string name, string acronym)
+		{
+			ChangeName(name);
+			ChangeAcronym(acronym);
+		}
+
+		public int StateId { get; private set; }
+		public string Acronym { get; private set; }
+		public string Name { get; private set; }
+		public virtual IReadOnlyCollection<Town> Towns { get => towns; }
+
+        #region - Methods -
+        public void ChangeName(string name)
+        {
+            Name = name;
+        }
+        public void ChangeAcronym(string acronym)
+        {
+            Acronym = acronym;
+        }
+        #endregion
+
+    }
+}
+
---
+++ IBGE.Api/Domain/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using IBGE.Domains.Core.ValueObjects;
+
+namespace IBGE.Api.Domains.Entities
+{
+    public class User
+    {
+        protected User() {}
+        public User(string email, string password)
+        {
+            ChangeEmail(email);
+            ChangePassword(password);
+        }
+
+        public int UserId { get; private set; }
+        public Email Email { get; private set; }
+        public Password Password { get; private set; }
+
+        #region - Methods -
+        public void ChangeEmail(string email) => Email = new Email(email);
+        public void ChangePassword(string password) => Password = new Password(password);
+        #endregion
+
+    }
+}
+
